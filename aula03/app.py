2a267c1cca82a5e2d789a7e700f440e890f5873b
Pauloa90 <paulo.a90@hotmail.com>
2020-02-20 14:00:26 +0000

    flask

--- aula03/app.py
+++ aula03/app.py
@@ -3,8 +3,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/hello/<nome>")
-def hello(nome):
+@app.route('/hello/')
+def hello(nome = ''):
     return "<h1> Hello {}</h1>".format(nome)
+
+@app.route('/blog/')
+@app.route('/blog/<int:postID>')
+def blog(postID = -1):
+    if postId >= 0:
+        return "blog info {}".format(postID)
+    else:
+        return "Blog todo"
 
 if __name__ == '__main__':
     app.run(debug=True)
